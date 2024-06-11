07980786434d6d9d960c8e4599a117544c6b4984
dsy <deng.siyang@outlook.com>
2024-06-11 15:05:05 +0800

    validation set

--- test.py
+++ test.py
@@ -11,17 +11,17 @@ model = BertForSequenceClassification.from_pretrained('bert_classifier')
 
 class_names = ['Normal', 'Anomalous']
 
-validation_data_filename = 'bgl_logs_train.json'
+validation_data_filename = 'test_logs_sub_1.json'
 
 with open(validation_data_filename, "r") as f:
     data = json.load(f)
-    random.shuffle(data)
-    total = 0
-    correct = 0
+    # random.shuffle(data)
+    # total = 0
+    # correct = 0
     results = []
     for i, log in tqdm(enumerate(data)):
-        if i > 199:
-            break
+        # if i > 199:
+        #     break
         encoded_input = tokenizer(log['log'], padding=True, truncation=True, return_tensors='pt')
         output = model(**encoded_input)
         predictions = output.logits.argmax(-1).item()
@@ -31,11 +31,11 @@ with open(validation_data_filename, "r") as f:
             'label': predictions_class,
             'index': log['index']
         })
-        if (predictions_class == log['label']):
-            correct += 1
+        # if (predictions_class == log['label']):
+        #     correct += 1
             # print(f'Correct: {log["index"]}, {log['label']}')
-        total += 1
-    print(f'Accuracy: {correct / total}')
+        # total += 1
+    # print(f'Accuracy: {correct / total}')
     with open('result.csv', 'w', newline='') as f:
         writer = csv.writer(f, delimiter=',')
         writer.writerows([[result['index'], result['label']] for result in results])
